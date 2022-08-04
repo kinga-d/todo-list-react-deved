95c1d268996800639d6fa576235bbaac352ac20a
Kinga Danielewicz <danielewicz.kinga@gmail.com>
2022-08-04 23:23:56 +0200

    completed button

--- todo-list-react-deved/src/components/Todo.js
+++ todo-list-react-deved/src/components/Todo.js
@@ -4,14 +4,24 @@ const Todo = ({ text, todo, setTodos, todos }) => {
     //Events
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id))
-};
+    };
+    const completeHandler = () => {
+        setTodos(todos.map(item => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fa-solid fa-circle-check"></i></button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}` }>{text}</li>
+            <button onClick={completeHandler} className="complete-btn"><i className="fa-solid fa-circle-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fa-solid fa-trash-can"></i></button>
         </div>
         );
 }
 
-export default Todo;
+export default Todo; 
